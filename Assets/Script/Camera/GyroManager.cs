6245162b45fa0a221ba8c4ee4fab2fffbb7ecbf8
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2022-05-25 17:05:49 +0200

    Fixe lerp rotation and gyroscope output

--- Assets/Script/Camera/GyroManager.cs
+++ Assets/Script/Camera/GyroManager.cs
@@ -2,100 +2,124 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GyroManager : MonoBehaviour
 {
     private Gyroscope _gyroscope;
     private bool isGyroEnable = false;
-    private ScreenOrientation _screenOrientation = ScreenOrientation.PORTRAIT;
-    private bool needToBeOriented = false;
-    
-    [Header("Debug Settings")]
-    [SerializeField] private GameObject rep;
+    private bool movementMode;
+    private DeviceOrientation _currentScreenOrientation = DeviceOrientation.Unknown;
 
-    [Header("Orientation settings")]
-    [SerializeField] [Range(0, 45)] private float minDegToPortrait;
-    [SerializeField] [Range(0, 45)] private float minDegToLandscape;
+    private CinemachineVirtualCamera m_vitualCamera;
 
-    [Header("Camera Anim Settings")]
-    [SerializeField] private float _timeAfterRotation;
+    private float curveTime;
+
+    private Vector3 lastPosition;
+    private bool isMovementEnable;
+
+    [Header("General Settings")]
+    [SerializeField] private GameObject player;
     [SerializeField] private AnimationCurve _sizeCurve;
-    [SerializeField] private float _sizeTime;
     [SerializeField] private AnimationCurve _rotationCurve;
-    [SerializeField] private float _rotationTime;
+
+    [Header("Movement Mode Settings")]
+    [SerializeField] private GameObject _targerCam;
+    [SerializeField] private float sensitivity = 2;
+    [SerializeField] private float _movementSize;
+    [SerializeField] private float _movementRotation;
+
+    [Header("Action Mode Settings")]
+    [SerializeField] private float _actionSize;
+    [SerializeField] private float _actionRotation;
     
 
     private void Start()
     {
-        if (SystemInfo.supportsGyroscope)
-        {
-            Debug.Log("Gyroscope Enable");
-
-            _gyroscope = Input.gyro;
-            _gyroscope.enabled = true;
-            isGyroEnable = true;
-        }
-        else
-            Debug.LogWarning("Gyroscope not supported by the platform");
+        m_vitualCamera = gameObject.GetComponent<CinemachineVirtualCamera>();
+        isGyroEnable = EnableGyro();
     }
 
     private void Update()
     {
         if (isGyroEnable)
         {
-            float zRotationPhone = (_gyroscope.attitude.eulerAngles.z + 90) % 360;
-
-            // DEBUG
-            rep.transform.rotation = Quaternion.Euler(new Vector3(0,0, -zRotationPhone));
-            // Debug.Log(zRotationPhone);
-
-            if (_screenOrientation == ScreenOrientation.PORTRAIT)
+            // Changement de mode : Portrait ou Paysage
+            if (_currentScreenOrientation != Input.deviceOrientation)
             {
-                if (zRotationPhone < 90 + minDegToLandscape)
+                curveTime += Time.deltaTime;
+
+                switch (Input.deviceOrientation)
                 {
-                    _screenOrientation = ScreenOrientation.LANDSCAPE_RIGHT;
-                    needToBeOriented = true;
+                    case DeviceOrientation.Portrait:
+                        OrientScreen(_actionSize, _actionRotation);
+                        movementMode = true;
+                        break;
+
+                    case DeviceOrientation.LandscapeLeft:
+                        OrientScreen(_movementSize, _movementRotation);
+                        movementMode = false;
+                        break;
                 }
-                else if (zRotationPhone > 270 - minDegToLandscape)
+            }
+
+            if(movementMode)
+            {
+                InputHandle();
+
+                if (isMovementEnable)
                 {
-                    _screenOrientation = ScreenOrientation.LANDSCAPE_LEFT;
-                    needToBeOriented = true;
+                    Vector3 movement = lastPosition - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    movement.z = 0;
+                    _targerCam.transform.position = _targerCam.transform.position + movement * sensitivity;
+
+                    lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 }
             }
             else
             {
-                if (zRotationPhone < 180 + minDegToPortrait && zRotationPhone > 180 - minDegToPortrait)
-                {
-                    _screenOrientation = ScreenOrientation.PORTRAIT;
-                    needToBeOriented = true;
-                }
-                else if(_screenOrientation == ScreenOrientation.LANDSCAPE_LEFT && zRotationPhone < 180)
-                {
-                    _screenOrientation = ScreenOrientation.LANDSCAPE_RIGHT;
-                    needToBeOriented = true;
-                }
-                else if (_screenOrientation == ScreenOrientation.LANDSCAPE_RIGHT && zRotationPhone > 180)
-                {
-                    _screenOrientation = ScreenOrientation.LANDSCAPE_LEFT;
-                    needToBeOriented = true;
-                }
+                _targerCam.transform.position = player.transform.position;
             }
-
-            if(needToBeOriented)
-                ChangeOrientation();
         }
     }
 
-    private void ChangeOrientation()
+    private void InputHandle()
     {
-        Debug.Log("Change orientation for " + _screenOrientation.ToString());
-        needToBeOriented = false;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            isMovementEnable = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            isMovementEnable = false;
+        }
     }
 
-    private enum ScreenOrientation
+    private void OrientScreen(float endSize, float endRotation)
     {
-        PORTRAIT,
-        LANDSCAPE_RIGHT,
-        LANDSCAPE_LEFT
+        m_vitualCamera.m_Lens.OrthographicSize = Mathf.Lerp(m_vitualCamera.m_Lens.OrthographicSize, endSize, _sizeCurve.Evaluate(curveTime));
+        m_vitualCamera.transform.rotation = Quaternion.Lerp(m_vitualCamera.transform.rotation, Quaternion.Euler(0, 0, endRotation), _sizeCurve.Evaluate(curveTime));
+
+        if (m_vitualCamera.m_Lens.OrthographicSize == endSize && m_vitualCamera.transform.rotation == Quaternion.Euler(0, 0, endRotation))
+        {
+            _currentScreenOrientation = Input.deviceOrientation;
+            curveTime = 0;
+        }
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            Debug.Log("Gyroscope Enable");
+            _gyroscope = Input.gyro;
+            _gyroscope.enabled = true;
+
+            return true;
+        }
+        Debug.LogWarning("Gyroscope not supported by the platform");
+        return false;
     }
 }
