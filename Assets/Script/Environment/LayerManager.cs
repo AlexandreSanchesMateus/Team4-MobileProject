34affe2b38415ddc3c6637526a2eef95e121c9d7
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2022-06-08 11:56:53 +0200

    Add more code

--- Assets/Script/Environment/LayerManager.cs
+++ Assets/Script/Environment/LayerManager.cs
@@ -5,50 +5,106 @@ using UnityEngine;
 public class LayerManager : MonoBehaviour
 {
     private int layerID = 0;
-    private Zone _inZone;
-    private bool takeChangerLayer = false;
+    private int layerIDtoGo = 0;
+    private Zone _inZone = null;
+    private bool securityZone = false;
+    [SerializeField] private int _startLayer = 0;
     [SerializeField] private float changeScale = 10f;
     [SerializeField] private List<Layer> layers = new List<Layer>();
 
 
     private void Start()
     {
-        for(int i = 0; i < layers.Count; i++)
+        foreach(Layer other in layers)
         {
-            layers[i].ground.enabled = false;
+            other.m_startScale = other.ground.transform.localScale;
         }
+
+        if(_startLayer == 0 && layers.Count > 1)
+        {
+            LoadLayer(1);
+        }
+
+        // les rétrécire celon l'ordre dans les layers
     }
 
     private void Update()
     {
-        Vector2 playerPosition = PlayerMovement2.Instance.gameObject.transform.position;
+        Vector2 playerPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Player position !!!!
 
         if (_inZone == null)
         {
             if (layerID == 0)
-                _inZone = CollideWithLayerZones(layers[layerID].m_zones, playerPosition);
+                _inZone = CollideWithLayerZones(layers[layerID].m_zones, playerPosition, 1);
             else if (layerID == layers.Count - 1)
-                _inZone = CollideWithLayerZones(layers[layerID - 1].m_zones, playerPosition);
+                _inZone = CollideWithLayerZones(layers[layerID - 1].m_zones, playerPosition, layerID - 1);
             else
             {
-                _inZone = CollideWithLayerZones(layers[layerID].m_zones, playerPosition);
-                if(_inZone == null)
-                    _inZone = CollideWithLayerZones(layers[layerID - 1].m_zones, playerPosition);
+                _inZone = CollideWithLayerZones(layers[layerID].m_zones, playerPosition, layerID + 1);
+
+                if (_inZone == null)
+                    _inZone = CollideWithLayerZones(layers[layerID - 1].m_zones, playerPosition, layerID - 1);
             }
+
+            // Changer le parallaxe
+
         }
         else
         {
-            
+            securityZone = _inZone.CollideWithSecurityZone(playerPosition);
+
+            if (_inZone.CollideWithZone(playerPosition))
+            {
+                float _stairPosition = (playerPosition.y - _inZone.bottomRightCorner.y) / (_inZone.topLeftCorner.y - _inZone.bottomRightCorner.y);
+
+                float xScaleIn = layers[layerID].m_startScale.x + changeScale;
+                float xScaleOut = layers[layerIDtoGo].m_startScale.x - changeScale;
+
+                // Agrandir
+                layers[layerID].ground.transform.localScale = Vector2.Lerp(layers[layerID].m_startScale, new Vector2(xScaleIn, (xScaleIn * layers[layerID].m_startScale.y) / layers[layerID].m_startScale.x), _stairPosition);
+                // Reduire
+                layers[layerIDtoGo].ground.transform.localScale = Vector2.Lerp(new Vector2(xScaleOut, (xScaleOut * layers[layerIDtoGo].m_startScale.y) / layers[layerIDtoGo].m_startScale.x), layers[layerID].m_startScale, _stairPosition);
+                
+                // doit changer le scale avec l'encre + tout ce derrière
+
+                Debug.Log("Stay");
+            }
+            else
+            {
+                Debug.Log("Exit");
+
+                if (securityZone)
+                {
+                    layerID = layerIDtoGo;
+                    securityZone = false;
+                }
+
+                _inZone = null;
+            }
         }
     }
 
-    private Zone CollideWithLayerZones(List<Zone> _zones, Vector2 position)
+    private void LoadLayer(int id)
+    {
+        float xScale;
+
+        if (id > layerID)
+            xScale = layers[id].m_startScale.x - changeScale;
+        else
+            xScale = layers[id].m_startScale.x + changeScale;
+
+        layers[id].ground.transform.localScale = new Vector2(xScale, (xScale * layers[id].m_startScale.y) / layers[id].m_startScale.x);
+    }
+
+    private Zone CollideWithLayerZones(List<Zone> _zones, Vector2 position, int _changeLayerId)
     {
         foreach (Zone other in _zones)
         {
             if (other.CollideWithZone(position) || other.CollideWithSecurityZone(position))
             {
                 Debug.Log("Enter");
+                layerIDtoGo = _changeLayerId;
                 return other;
             }
         }
